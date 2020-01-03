2f29e11f9a8a2e65b91200a6b413eefcde9d4c28
L <L@LAPTOP-HBVP71NL>
2020-01-03 11:50:04 +0800

    1

--- TestPractice/src/com/test/practice.java
+++ TestPractice/src/com/test/practice.java
@@ -3,7 +3,7 @@ package com.test;
 public class practice {
 	public static void main(String[] args) {
 			String s = "nbbbbn";
-			
+			System.out.println("分支测试");
 			System.out.println(Math.rint(22.5));	
 	}
 }
